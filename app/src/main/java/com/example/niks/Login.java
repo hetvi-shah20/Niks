ba5b221a495d995371c45cf5b0018aac97bc4720
hetvi <hetvishah20898@gmail.com>
2019-04-23 21:20:59 +0530

    Signup and otp

--- app/src/main/java/com/example/niks/Login.java
+++ app/src/main/java/com/example/niks/Login.java
@@ -1,13 +1,25 @@
 package com.example.niks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+    TextView signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        signup =  findViewById(R.id.txtNotAccount);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Login.this,Signup.class);
+                startActivity(i);
+            }
+        });
     }
 }
