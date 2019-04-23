aa975965222f90046fc4d365927c46a1e3f4dcef
hetvi <hetvishah20898@gmail.com>
2019-04-23 13:24:41 +0530

    Intial commit

---
+++ app/src/main/java/com/example/niks/SplashScreen.java
@@ -0,0 +1,51 @@
+package com.example.niks;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    ImageView splash;
+    Animation rotate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+
+        splash =  findViewById(R.id.splash);
+        rotate = AnimationUtils.loadAnimation(SplashScreen.this,R.anim.rotation);
+        splash.startAnimation(rotate);
+        rotate.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                finish();
+                Intent i = new Intent(SplashScreen.this,Login.class);
+                startActivity(i);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
