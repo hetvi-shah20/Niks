24f11d999c6b5aff7c6047c3f099f8cb8827f13e
hetvi <hetvishah20898@gmail.com>
2019-04-24 11:50:47 +0530

    changes

--- app/src/main/java/com/example/niks/Login.java
+++ app/src/main/java/com/example/niks/Login.java
@@ -7,18 +7,27 @@ import android.view.View;
 import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
-    TextView signup;
+    TextView signup,forgotpwd;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         signup =  findViewById(R.id.txtNotAccount);
+        forgotpwd =  findViewById(R.id.txtForgotpwd);
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(Login.this,Signup.class);
                 startActivity(i);
+            }
+        });
+        forgotpwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent  i2= new Intent(Login.this,ForgotPassword.class);
+                startActivity(i2);
             }
         });
     }
